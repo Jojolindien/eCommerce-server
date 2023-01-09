ac0358502e45cb36bbcdf9687e77d6a5c9b5fcf4
stephane <stephane.schwender@gmail.com>
2023-01-09 20:01:54 +0100

    Category Create and List

--- routes/category.js
+++ routes/category.js
@@ -15,7 +15,7 @@ const {
 } = require("../controllers/category");
 
 //routes
-router.post("/category", create);
+router.post("/category", authCheck, adminCheck, create);
 router.get("/categories", list);
 router.get("/category/:slug", read);
 router.put("/category/:slug", authCheck, adminCheck, update);
