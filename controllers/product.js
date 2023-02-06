554eae8dc2d1b9cf427a56e93fcc6b887b37b022
stephane <stephane.schwender@gmail.com>
2023-02-06 15:59:51 +0100

    endpoint pagination Product

--- controllers/product.js
+++ controllers/product.js
@@ -71,15 +71,38 @@ exports.update = async (req, res) => {
   }
 };
 
+//Without pagination
+// exports.list = async (req, res) => {
+//   try {
+//     //createdAt/updatedAt, desc/asc, 3
+//     const { sort, order, limit } = req.body;
+//     const products = await Product.find({})
+//       .populate("category")
+//       .populate("subCategories")
+//       .sort([[sort, order]])
+//       .limit(limit)
+//       .exec();
+
+//     res.json(products);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+//With Pagination
 exports.list = async (req, res) => {
   try {
     //createdAt/updatedAt, desc/asc, 3
-    const { sort, order, limit } = req.body;
+    const { sort, order, page } = req.body;
+    const currentPage = page || 1;
+    const perPage = 3;
+
     const products = await Product.find({})
+      .skip((currentPage - 1) * perPage)
       .populate("category")
       .populate("subCategories")
       .sort([[sort, order]])
-      .limit(limit)
+      .limit(perPage)
       .exec();
 
     res.json(products);
